bf033916647b918b1aaf66fa11b30b8435566ffc
vi35 <vi35@cornell.edu>
2018-10-20 22:02:46 -0400

    Implement landing page front-end

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('includes/init.php');
 // echo phpInfo();
 ?>
 <!DOCTYPE html>
-<html>
+<html class="uk-height-1-1">
 
 <head>
   <meta charset="UTF-8" />
@@ -16,16 +16,30 @@ include('includes/init.php');
   <script src="scripts/jquery-ui.min.js" type="text/javascript"></script>
   <script src="scripts/activity.js" type="text/javascript"></script>
   <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.19/css/uikit.min.css" />
 
+  <!-- UIkit JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.19/js/uikit.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.19/js/uikit-icons.min.js"></script>
   <title>Share Here- Sign In</title>
 </head>
 
-<body>
 
-  <h1> ShareEarn </h1>
-  <p class = "slogan"> Envision what you can do for a better future for anyone. </p>
+<body class="uk-height-1-1">
+  <div class="content-wrapper uk-container uk-background-cover">
+    <div class="">
+      <h class="uk-heading-primary"; id="title-text">Our title goes here</h>
+      <p>Some bullshit quote</p>
+    </div>
+    <div class="circle"></div>
+    <div class="">
+      <button class="uk-button uk-button-default">Button 1</button>
+      <button class="uk-button uk-button-default">Button 2</button>
+    </div>
 
-  <?php
+
+  <!-- <?php
   if (!$current_user) {
     ?>
     <form action="index.php" method="post">
@@ -53,6 +67,13 @@ include('includes/init.php');
     <?php
   }
   ?>
-<p> Don't have an account? Click <a href="signup.php"> here </a> to sign up now! </p>
+<p> Don't have an account? Click <a href="signup.php"> here </a> to sign up now! </p> -->
+<div class="footer uk-flex">
+  <img class="succ" data-src="images/boi1-01.png" uk-img></img>
+  <img class="succ" data-src="images/boi2-02.png" uk-img></img>
+  <img class="succ" data-src="images/boi3-03.jpg" uk-img></img>
+  <img class="succ" data-src="images/brhacks4-04.png" uk-img></img>
+</div>
+</div>
 </body>
 </html>
